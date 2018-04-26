2f8527dbafcbd147dff8f5dd6d8891e74206de5f
Mirela Dedic <mdedic1@etf.unsa.ba>
2018-04-27 00:48:13 +0200

    NavBar-View,Model

---
+++ Projekat1/Gbook/Gbook/Model/Bibliotekar.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * created by:Mirela Dedic
+ * created on: 26.04.2018
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gbook.Model
+{
+    class Bibliotekar : Zaposlenik
+    {
+        private List<Clan> clanovi;
+        private List<Knjiga> knjige;
+        public Bibliotekar(OsobaINFO info, DateTime datumZaposlenja, double plata) : base(info, datumZaposlenja, plata) {
+            clanovi = new List<Clan>();
+            knjige = new List<Knjiga>();
+        }
+       
+    }
+}
---
+++ Projekat1/Gbook/Gbook/Model/Knjiga.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * created by:Mirela Dedic
+ * created on: 26.04.2018
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gbook.Model
+{
+    class Knjiga
+    {
+        private string naziv;
+        private string autor;
+        private string izdavac;
+        private long ISBN;
+        private DateTime godinaIzdanja;
+        private int naStanju;
+        private Boolean zaduzena;
+
+        public Knjiga(string naziv, string autor, string izdavac, long iSBN, DateTime godinaIzdanja, int naStanju, bool zaduzena)
+        {
+            this.naziv = naziv;
+            this.autor = autor;
+            this.izdavac = izdavac;
+            ISBN = iSBN;
+            this.godinaIzdanja = godinaIzdanja;
+            this.naStanju = naStanju;
+            this.zaduzena = zaduzena;
+        }
+    }
+}
