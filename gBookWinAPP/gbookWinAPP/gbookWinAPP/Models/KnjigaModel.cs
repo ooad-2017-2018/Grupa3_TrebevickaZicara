48f3e5246ea0cbb35e0a7937d7bde0a61b3692a7
mireladedic <mdedic1@etf.unsa.ba>
2018-06-04 23:53:00 +0200

    DBConnect

--- gBookWinAPP/gbookWinAPP/gbookWinAPP/Models/KnjigaModel.cs
+++ gBookWinAPP/gbookWinAPP/gbookWinAPP/Models/KnjigaModel.cs
@@ -12,25 +12,40 @@ namespace gbookWinAPP.Models
     [Table("OOADKnjigaModel")]
     public class KnjigaModel
     {
-        public KnjigaModel()
-        {
+        /*   public KnjigaModel()
+           {
+               Get_CLIIDS = new HashSet<KnjigaModel>();
+           }
+           public virtual ICollection<KnjigaModel> Get_CLIIDS { get; set; }
 
-        }
+           [Required]
+           [Key]
+           [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+           public int KnjigaID { get; set; }
 
-        [Required]
+           public int ClanID { get; set; }
+
+           [ForeignKey("ClanID")]
+           // Navigation properties 
+           public virtual ClanModel ClanModel { get; set; }
+
+       */
+
         [Key]
+        [Column(Order = 0)]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int KorisnikKnige { get; set; }
+
+        [Key]
+        [Column(Order = 1)]
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int KnjigaID { get; set; }
 
-        [DatabaseGenerated(DatabaseGeneratedOption.None)]
-        [ForeignKey ("ClanID")]
-        public int ClanID { get; set; }
+        public virtual ClanModel OOADClanovi { get; set; }
+
+        public virtual KnjigaModel OOADKnjige { get; set; }
 
 
-
-        // Navigation properties 
-        public virtual ClanModel ClanModel { get; set; }
-  
 
         [Required]
         [StringLength(255)]
